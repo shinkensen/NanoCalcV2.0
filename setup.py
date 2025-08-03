bbcc919ffdaaf90e3cd943d33321f61456d291bd
shinkensen <noobywinner587@gmail.com>
2025-08-03 15:51:00 -0500

    BRO IMANA CRASH OUT, FINAL FIX

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import sys
 from cx_Freeze import setup, Executable
 
-base="Win32GUI"
+# Only use GUI mode on Windows to suppress console
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
 setup(
     name="NanoCalc",
     version="2.0",
     description="Efficient Calculator App",
-    executables=[Executable("main.py")],
+    executables=[Executable("main.py", base=base)]
 )
